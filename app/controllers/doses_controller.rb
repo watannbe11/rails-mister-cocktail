4f41f19b0780f9eb714c7c949f25974e92439438
yuichiro watanabe <yuuichiro1139@gmail.com>
2020-02-21 17:12:36 +0900

    changing stylr

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -26,6 +26,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id )
   end
 end
